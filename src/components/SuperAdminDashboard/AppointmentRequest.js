b5a5241a04068b1611aaca36a52b48f5db37a877
Precious Enyidi <precious.enyidi@esolutiontec.com>
2025-02-04 15:14:01 +0100

    Appointment

--- src/components/SuperAdminDashboard/AppointmentRequest.js
+++ src/components/SuperAdminDashboard/AppointmentRequest.js
@@ -10,8 +10,8 @@ const AppointmentRequests = () => (
   <div className="w-full shadow-md rounded-md mt-4 flex flex-col justify-start items-center px-4 py-4 bg-white">
     <h3 className="text-[#1A4700] font-bold text-left w-full">Appointment Requests</h3>
 
-    <div className="w-full py-2">
-      <table className="w-full">
+    <div className="w-full py-2 flex flex-col justify-center items-start">
+      <table className="w-11/12">
           <thead>
             <tr>
               <td className="px-3 py-2 text-sm text-center font-bold text-[#555555]">Name</td>
@@ -22,7 +22,7 @@ const AppointmentRequests = () => (
           </thead>
           <tbody>
             {requests.map((req, index) => (
-              <tr key={index} className="rounded-full shadow-md w-full mt-4">
+              <tr key={index} className="rounded-full w-full mt-4">
                 <td className="px-3 py-3 font-bold text-sm text-[#1A4700] text-center">{req.name}</td>
                 <td className="px-3 py-3 font-bold text-sm text-[#1A4700] text-center">{req.disease}</td>
                 <td className="px-3 py-3 font-bold text-sm text-[#1A4700] text-center">{req.date}</td>
